624c2305bc14ea4075aa623b55d00add028b5d5d
Nick Chin <44158435+hermitP@users.noreply.github.com>
2019-04-20 15:23:57 +0800

    Update century_problem.ml

--- lib/century_problem.ml
+++ lib/century_problem.ml
@@ -3,6 +3,15 @@ type expr =
   | Multi
   | Num of int;;
 
+let a = [Num (1); Num (2); Add; Num(3); Num (4); Add;
+         Num (5); Multi; Num (6); Add; Num (7); Add;
+         Num (8); Add; Num (9)];;
+
+let b = [Num (1); Add; Num (2); Multi; Num (3); Add;
+         Num (4); Add; Num (5); Add; Num (6); Num (7);
+         Add; Num (8); Add; Num (9)];;
+
+
 let conjoin_num (ls : expr list) =
   let rec walk l acc_num acc_ls  =
     match l with
@@ -12,6 +21,8 @@ let conjoin_num (ls : expr list) =
     | Multi :: t -> walk t 0 (Multi :: Num (acc_num) :: acc_ls)
   in
   walk ls 0 [];;
+
+conjoin_num a;;
 
 let multi_num (ls : expr list) =
   let rec walk l acc =
@@ -27,6 +38,8 @@ let multi_num (ls : expr list) =
   in
   walk ls [];;
 
+multi_num @@ conjoin_num a;;
+
 let add_num (ls : expr list) =
   let rec walk l acc =
     match l with
@@ -36,13 +49,31 @@ let add_num (ls : expr list) =
   in
   walk ls 0;;
 
-let a = [Num (1); Num (2); Add; Num(3); Num (4); Add;
-         Num (5); Multi; Num (6); Add; Num (7); Add;
-         Num (8); Add; Num (9)];;
-
-let b = [Num (1); Add; Num (2); Multi; Num (3); Add;
-         Num (4); Add; Num (5); Add; Num (6); Num (7);
-         Add; Num (8); Add; Num (9)];;
 
 add_num @@ multi_num @@ conjoin_num a;;
 add_num @@ multi_num @@ conjoin_num b;;
+
+let eval_expr ls =
+  add_num @@ multi_num @@ conjoin_num ls;;
+
+let gen_candidates _ =
+  let rec apply_expr lst n =
+    match lst with
+    | [] -> []
+    | x -> [Num (n) :: Add :: x; Num (n) :: Multi :: x; Num (n) ::x]
+  in
+
+  let rec apply_num_and_expr big_list n =
+    if n = 0 then big_list else
+    match big_list with
+    | [] -> apply_num_and_expr ([Num (n)] :: []) (n - 1)
+    | x ->
+       apply_num_and_expr (List.fold_left (fun i h -> (apply_expr h n) @ i) [] big_list) (n - 1)
+  in
+  apply_num_and_expr [] 9;;
+
+
+let century_candidates = 
+  List.filter (fun x -> eval_expr x = 100) (gen_candidates ());;
+
+List.length century_candidates;;
