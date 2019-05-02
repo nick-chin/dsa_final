09a6ebbc7df10f38c661aadebac108221f82e585
xinyuluo <luoxinyu06@gmail.com>
2019-05-02 21:07:52 +0800

    clean up the code

--- lib/watchman_problem.ml
+++ lib/watchman_problem.ml
@@ -100,15 +100,15 @@ end
 (************************************************)
 (* get the maximum element in a list *)
 let max_list l =
-  List.fold_left (fun acc x -> max acc x) (float_of_int min_int) l;;
+  List.fold_left (fun acc x -> max acc x) (float_of_int min_int) l
 
 (* get the minimum element in a list *)
 let min_list l =
-  List.fold_left (fun acc x -> min acc x) (float_of_int max_int) l;;
+  List.fold_left (fun acc x -> min acc x) (float_of_int max_int) l
 
 (* check if a 1 x 1 square given by p (its left-bottom corner) is inside the room *)
 let square_inside_room room p =
-  point_within_polygon room ((++) p (0.5, 0.5));;
+  point_within_polygon room ((++) p (0.5, 0.5))
 
 let get_neighbours room p =
     let Point (x, y) = p in
@@ -135,11 +135,11 @@ let get_all_squares room =
     done;
     x := !x +. 1.
   done;
-  !res;;
+  !res
 
 (* get the Euclidean distance between two points *)
 let distance a b =
-  vec_length ((--) a b);;
+  vec_length ((--) a b)
 
 (* get the neighbours of a square p inside the room that can be reached by light *)
 let get_lightable_neighbours room p =
@@ -163,14 +163,14 @@ let get_lightable_neighbours room p =
       true
   in
   let lightable_neighbours = List.filter (fun e -> lightable p e) neighbours in
-  lightable_neighbours;;
+  lightable_neighbours
 
 (* get the possible moves from a square p inside the room *)
 let get_next_moves room p =
   let Point (x, y) = p in
   let candidates = [Point (x +. 1., y); Point (x, y +. 1.); Point (x -. 1., y); Point (x, y -. 1.)] in
   let next_moves = List.filter (fun e -> square_inside_room room e) candidates in
-  next_moves;;
+  next_moves
 
 (* hash table to record lighted squares *)
 module SquareTable =
@@ -188,11 +188,11 @@ let count_new_lighted_squares room lighted_squares p =
     | _ -> true
   in let open SquareTable in
   let new_lighted = List.filter (fun e -> not (check_lighted (get lighted_squares e))) neighbours in
-  List.length new_lighted;;
+  List.length new_lighted
 
 (* check if moving to point p lights up new squares *)
 let light_new_squares room lighted_squares p =
-    count_new_lighted_squares room lighted_squares p <> 0;;
+    count_new_lighted_squares room lighted_squares p <> 0
 
 (* compare two moves by the number of potential newly lighted squares in reverse order *)
 let comp_move_new_lighted room lighted_squares p1 p2 =
@@ -200,7 +200,7 @@ let comp_move_new_lighted room lighted_squares p1 p2 =
   let new2 = count_new_lighted_squares room lighted_squares p2 in
   if new1 < new2 then 1
   else if new1 > new2 then (-1)
-  else 0;;
+  else 0
 
 (* build a hashtable out of a list of directions in the order of preference priority *)
 let get_pref_table preferences p =
@@ -217,7 +217,7 @@ let get_pref_table preferences p =
     else if direction = "left"
     then insert pref_table ((++) p (-1., 0.)) i
   done;
-  pref_table;;
+  pref_table
 
 (* choose the next move greedily, and if two moves potentially light the same number of new squares, choose according to a priority list of directions; if we cannot light new squares by moving in any of the four directions, we return None to indicate that we are trapped and the procedure needs to restart in another (possibly distant) unlighted square *)
 let choose_next_move room p preferences lighted_squares =
@@ -244,9 +244,9 @@ let choose_next_move room p preferences lighted_squares =
      if light_new_squares room lighted_squares h
      then Some h
      else None
-  | _ -> None;;
+  | _ -> None
 
-module PointTable = ResizableListBasedHashTable(struct type t = Point end);;
+module PointTable = ResizableListBasedHashTable(struct type t = Point end)
 
 (* greedy & fixed order *)
 let get_watchman_path room =
@@ -330,36 +330,14 @@ let get_watchman_path room =
         List.iter (fun e -> insert lighted_squares e true) (uniq (!p :: !neighbours));
       end
   done;
-  List.rev !path;;
-
-(*
-open TestRooms;;
-let room = room1;;
-let p = ref (Point (0., 0.));;
-let path = ref [!p];;
-let neighbours = ref (get_lightable_neighbours room !p);;
-let all_squares = get_all_squares room;;
-let num_of_squares = List.length all_squares;;
-open SquareTable;;
-let lighted_squares = mk_new_table 5;;
-List.iter (fun e -> insert lighted_squares e true) (uniq (!p :: !neighbours));;
-let preferences = [|"up"; "right"; "down"; "left"|];;
-(!(lighted_squares.size) < num_of_squares);;
-let next_move = choose_next_move room !p preferences lighted_squares;;
-next_move <> None;;
-p := get_exn next_move;;
-neighbours := get_lightable_neighbours room !p;;
-path := !p :: !path;;
-List.iter (fun e -> insert lighted_squares e true) (uniq (!p :: !neighbours));;
-List.rev !path;;
- *)
+  List.rev !path
   
 (************************************************)
 (*                   2. Tests                   *)
 (************************************************)
 (* path starts at (0, 0) *)
 let test_start_at_origin path =
-  List.hd path = Point (0., 0.);;
+  List.hd path = Point (0., 0.)
 
 (* path is connected *)
 let test_connectivity room path =
@@ -369,12 +347,12 @@ let test_connectivity room path =
        walk (h2 :: t) ((List.mem h2 (get_neighbours room h1)) && acc)
     | _ -> acc
   in
-  walk path true;;
+  walk path true
 
 (* path does not go through the walls or go outside the room *)
 let test_no_collisions room path =
   (* it suffices to check that all squares that the watchman stepped on in the path are inside the room *)
-  List.for_all (fun e -> square_inside_room room e) path;;
+  List.for_all (fun e -> square_inside_room room e) path
 
 (* the whole room is lighted in the end *)
 let test_full_room_coverage room path =
@@ -391,8 +369,7 @@ let test_full_room_coverage room path =
   in
   let lighted = walk path (mk_new_table 5) in
   let num_of_squares = List.length (get_all_squares room) in
-  !(lighted.size) = num_of_squares;;
-
+  !(lighted.size) = num_of_squares
 
 (* test if the path is valid *)
 let test_valid_path room =
@@ -401,7 +378,7 @@ let test_valid_path room =
   assert (test_connectivity room p);
   assert (test_no_collisions room p);
   assert (test_full_room_coverage room p);
-  true;;
+  true
 
 (* get the edges of a polygon *)
 let get_edges polygon =
@@ -411,8 +388,7 @@ let get_edges polygon =
        walk (h2 :: t) ((h1, h2) :: edges)
     | _ -> edges
   in
-  walk polygon [];;
-
+  walk polygon []
 
 (* randomly choose the next point in the polygon *)
 let generate_next_point p edgeLength =
@@ -433,9 +409,8 @@ let generate_next_point p edgeLength =
   else
     (* go left *)
     p1 := (++) p (-.edgeLength, 0.);
-  !p1;;
-
-
+  !p1
+  
 (* generate a random polygon containing the square (0, 0) in the first quadrant *)
 let generate_random_polygon n turnsLimit =
   let p = ref (Point (0., 0.)) in
@@ -476,12 +451,11 @@ let generate_random_polygon n turnsLimit =
       else ();
       polygon := (Point (0., get_y high_point)) :: high_point :: !polygon;
     end;
-  List.rev (!polygon);;
+  List.rev (!polygon)
 
 (* flip a polygon vertically *)
 let flip poly =
-  List.map (fun e -> if (=~=) (get_y e) 0. then Point (get_x e, 0.) else Point (get_x e, -.(get_y e))) poly;;
-
+  List.map (fun e -> if (=~=) (get_y e) 0. then Point (get_x e, 0.) else Point (get_x e, -.(get_y e))) poly
 
 (* generate a random room *)
 let generate_random_room _ =
@@ -502,17 +476,16 @@ let generate_random_room _ =
     | h :: t -> walk t l2 (h :: acc)
   in
   let final_poly = List.tl @@ List.rev shifted_poly2 in
-  List.rev (walk poly1 (final_poly @ [List.nth shifted_poly2 (len2 - 1)]) []);;
+  List.rev (walk poly1 (final_poly @ [List.nth shifted_poly2 (len2 - 1)]) [])
 
-        
 (* random rooms test *)
-
 let test_random_rooms _ =
   for _ = 1 to 5 do
     let room = generate_random_room () in
     assert (test_valid_path room)
   done;
-  true;;
+  true
+  
 (************************************************)
 (*                3. Output file                *)
 (************************************************)
@@ -533,13 +506,7 @@ let room_list = [room1; room2; room3; room4; room5; room6; room7; room8; room9; 
 let get_solution_list ls filename =
   let i = ref 0 in
   let s_lst = List.map (fun x -> i := !i + 1; string_of_int !i ^ ": " ^ get_solution_as_string x) ls in
-  write_strings_to_file filename s_lst;;
-
-
-
-  
-
-
+  write_strings_to_file filename s_lst
 
 
 (************************************************)
@@ -547,13 +514,12 @@ let get_solution_list ls filename =
 (************************************************)
 (* see report *)
 
-
 (************************************************)
 (*               5. Visualization               *)
 (************************************************)
 
 (* draw blank window without axes *)
-let draw_canvas _ = open_graph " 800x600";;
+let draw_canvas _ = open_graph " 800x600"
 
 (* get the scaling factor that best visualizes the room *)
 let get_scaling_factor room =
@@ -562,7 +528,7 @@ let get_scaling_factor room =
   let (min_x, min_y) = (min_list all_x, min_list all_y) in
   let (max_x, max_y) = (max_list all_x, max_list all_y) in
   let scaling_factor = 580. /. (max (max_x -. min_x) (max_y -. min_y)) in
-  scaling_factor;;
+  scaling_factor
 
 (* get the position of the origin (0, 0) that best visualizes the room *)
 let get_origin room =
@@ -580,7 +546,7 @@ let calibrate p scaling_factor origin =
   let Point (x0, y0) = origin in
   let x = x0 +. (get_x p) *. scaling_factor in
   let y = y0 +. (get_y p) *. scaling_factor in
-  Point (x, y);;
+  Point (x, y)
 
 (* get the center of the room *)
 let get_center room =
@@ -591,7 +557,7 @@ let get_center room =
   let (min_x, min_y) = (min_list all_x, min_list all_y) in
   let (max_x, max_y) = (max_list all_x, max_list all_y) in
   let center = (Point ((max_x +. min_x)/.2., (max_y +. min_y)/.2.)) in
-  calibrate center scaling_factor origin;;
+  calibrate center scaling_factor origin
 
 (* draw a room scaled by the scaling factor *)
 let draw_room room =
@@ -601,23 +567,7 @@ let draw_room room =
   set_color black;
   fill_poly (Array.of_list (List.map (fun e ->
                                 let Point (x, y) = calibrate e scaling_factor origin in
-                                (int_of_float x, int_of_float y)) room));;
-(*
-open TestRooms;;
-draw_canvas ();;
-draw_room room1;;
-draw_room room2;;
-draw_room room3;;
-draw_room room4;;
-draw_room room5;;
-draw_room room6;;
-draw_room room7;;
-draw_room room8;;
-draw_room room9;;
-draw_room room10;;
-clear_graph ();;
- *)
-
+                                (int_of_float x, int_of_float y)) room))
 
 (* cast light in the 1 x 1 square whose left-bottom corner is p *)
 let cast_light p scaling_factor origin =
@@ -626,24 +576,24 @@ let cast_light p scaling_factor origin =
   set_color yellow;
   fill_rect (int_of_float x) (int_of_float y) sf sf;
   set_color black;
-  draw_rect (int_of_float x) (int_of_float y) sf sf;;
+  draw_rect (int_of_float x) (int_of_float y) sf sf
 
 (* return the center of a square given by its lower left corner (scaled) *)
 let square_center p scaling_factor origin =
   let Point (x, y) = p in
   let center = Point (x +. 0.5, y +. 0.5) in
-  calibrate center scaling_factor origin;;
+  calibrate center scaling_factor origin
 
 (* check if a square given by its lower left corner is already lighted *)
 let lighted p scaling_factor origin =
   let Point (x, y) = square_center p scaling_factor origin in
-  point_color (int_of_float x) (int_of_float y) <> black;;
+  point_color (int_of_float x) (int_of_float y) <> black
 
 (* cast light around a watchman's position without letting the light go outside the room *)
 let cast_light_around room p scaling_factor origin =
   let neighbours = get_lightable_neighbours room p in
   List.iter (fun e -> if (square_inside_room room e) && (not (lighted e scaling_factor origin))
-                      then cast_light e scaling_factor origin) (p :: neighbours);;
+                      then cast_light e scaling_factor origin) (p :: neighbours)
 
 (* draw a red line between points a and b (scaled) *)
 let draw_line a b scaling_factor origin =
@@ -651,19 +601,19 @@ let draw_line a b scaling_factor origin =
   let a' = calibrate a scaling_factor origin in
   let b' = calibrate b scaling_factor origin in
   moveto (int_of_float (get_x a')) (int_of_float (get_y a'));
-  lineto (int_of_float (get_x b')) (int_of_float (get_y b'));;
+  lineto (int_of_float (get_x b')) (int_of_float (get_y b'))
 
 (* draw the watchman as a red dot in a square given by its lower left corner (scaled) *)
 let draw_watchman p scaling_factor origin =
   set_color red;
   let Point (x, y) = square_center p scaling_factor origin in
-  fill_circle (int_of_float x) (int_of_float y) (int_of_float (scaling_factor *. 0.3));;
+  fill_circle (int_of_float x) (int_of_float y) (int_of_float (scaling_factor *. 0.3))
 
 (* remove the watchman from a square given by its lower left corner (scaled) *)
 let remove_watchman p scaling_factor origin =
   set_color yellow;
   let Point (x, y) = square_center p scaling_factor origin in
-  fill_circle (int_of_float x) (int_of_float y) (int_of_float (scaling_factor *. 0.3));;
+  fill_circle (int_of_float x) (int_of_float y) (int_of_float (scaling_factor *. 0.3))
 
 (* visualize a watchman's route as he walks (scaled) *)
 
@@ -766,22 +716,7 @@ let visualize room =
         List.iter (fun e -> insert lighted_squares e true) (uniq (!p :: !neighbours));
       end
   done;
-  List.rev !path;;
-
-open TestRooms;;
-(* cannot put mk_screen () inside the function, other wise will raise error:
-Exception: (Unix.Unix_error "Interrupted system call" select "").
-Raised by primitive operation at unknown location
-Called from file "thread.ml", line 74, characters 23-50
-Called from file "//toplevel//", line 265, characters 2-17
-Called from file "toplevel/toploop.ml", line 180, characters 17-56
- *)
-(*
-draw_canvas ();;
-visualize room1;;
-clear_graph();;
- *)
-
+  List.rev !path
 
 (************************************************)
 (*                6. Video game                 *)
@@ -848,9 +783,4 @@ let play_game room =
       draw_string ("GAME FINISHED. SCORE: "^(string_of_int score));
   in
   play ();
-  !path;;
-
-open TestRooms;;
-(*play_game room1;;
-play_game room2;;
-*)
+  !path
