01e4242782135e62505e7bb0c23ae0a9e4129f2c
largecats <linfanxiaolinda@outlook.com>
2019-05-02 20:27:49 +0800

    Update ex3_draft.ml
    corrected typo in get_neighbours

--- lib/ex3_draft.ml
+++ lib/ex3_draft.ml
@@ -143,7 +143,7 @@ let get_neighbours room p =
 (* get the neighbours of a square p inside the room that can be reached by light *)
 let get_lightable_neighbours room p =
   let Point (x, y) = p in
-  let neighbours = let neighbours = get_neighbours room p in
+  let neighbours = get_neighbours room p in
   (* check if a neighbour is lightable from p (light cannot go around corners) *)
   let lightable p neighbour =
     if neighbour = Point (x +. 1., y +. 1.)
